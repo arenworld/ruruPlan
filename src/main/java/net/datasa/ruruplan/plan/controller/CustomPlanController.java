8bb774a4e64922abf16830fe2bca8c197df97afb
ArenWorld <160742032+arenworld@users.noreply.github.com>
2024-09-05 17:12:49 +0900

    [유지영] task 테이블 수정사항 반영

--- src/main/java/net/datasa/ruruplan/plan/controller/CustomPlanController.java
+++ src/main/java/net/datasa/ruruplan/plan/controller/CustomPlanController.java
@@ -2,9 +2,14 @@ package net.datasa.ruruplan.plan.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.datasa.ruruplan.plan.service.CustomPlanService;
+import net.datasa.ruruplan.plan.domain.dto.PlanDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * GPT추천일정을 내 일정으로 담은 후 개별수정하는 컨트롤러
@@ -16,8 +21,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("custom")
 public class CustomPlanController {
 
+    private final CustomPlanService customPlanService;
+
     @GetMapping("")
-    public String customPlan() {
+    public String customPlan(Model model) { //@RequestParam("planNum") Integer planNum) 지금은 테스트로 직접 넣고 나중에, planNum을 파람으로 받음
+        PlanDTO planDTO = customPlanService.getPlan();
+        model.addAttribute("planDTO", planDTO);
+        log.debug("출력할 플래너값: {}", planDTO);
         return "customView/customPlan";
     }
 }

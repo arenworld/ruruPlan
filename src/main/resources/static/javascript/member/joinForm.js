2de6b13927b7179260dd1785155e44ea1a95c9f1
maya <happymomo53@gmail.com>
2024-09-06 12:06:52 +0900

    [김유나] 1. joinForm.js 아이디, 이메일 에이젝스 관련 수정 (아직 해결 못함)

--- src/main/resources/static/javascript/member/joinForm.js
+++ src/main/resources/static/javascript/member/joinForm.js
@@ -12,16 +12,16 @@ $(document).ready(function() {
     });
     //모달창 열기
     $('.agree').on('click', function(event) {
-    event.preventDefault();
-    $('.terms').addClass('open'); // terms 모달 창 열기
-    $('.modal-backdrop').addClass('open'); // 배경 활성화
-});
+        event.preventDefault();
+        $('.terms').addClass('open'); // terms 모달 창 열기
+        $('.modal-backdrop').addClass('open'); // 배경 활성화
+    });
 
     // 모달 닫기
     $('.modal-backdrop').on('click', function() {
-    $('.terms').removeClass('open'); // terms 모달 창 닫기
-    $('.modal-backdrop').removeClass('open'); // 배경 비활성화
-});
+        $('.terms').removeClass('open'); // terms 모달 창 닫기
+        $('.modal-backdrop').removeClass('open'); // 배경 비활성화
+    });
 
     // 아이디 유효성 검사
     $('#memberId').keyup(idConfirm);
@@ -42,155 +42,176 @@ $(document).ready(function() {
 });
 
     // 아이디 유효성 겁사
-    function idConfirm(){
+function idConfirm() {
     //정규식   (문자열, 영어(필수)+숫자(선택) 6~15자, 대소문자 구분)
     id_regex = /^(?=.*[a-zA-Z])[a-zA-Z0-9]{6,15}$/;
     let id = $('#memberId').val();
 
     /*아이디 형식 확인*/
     if (!id_regex.test(id)) {
-    $('#idmsg').css('color', 'red');
-    $('#idmsg').html('아이디는 영문,숫자로 6~15글자 입력해 주세요');
-    return false;
-} else {
+        $('#idmsg').css('color', 'red');
+        $('#idmsg').html('아이디는 영문,숫자로 6~15글자 입력해 주세요');
+        return false;
+    }
+
     /*아이디 중복확인*/
-    $.ajax({
-    url: 'idDuplicate',
-    type: 'post',
-    data: {id: id},
-    success: function (res) {
-    if (res) {
-    $('#idmsg').css('color', 'red');
-    $('#idmsg').html("이미 사용중인 ID입니다");
-    return false;
-} else {
-    $('#idmsg').css('color', 'blue');
-    $('#idmsg').html("사용가능한 ID입니다.");
-    return true;
-}
-},
-    error: function () {
-    alert('IdDuplicate error');
-    return false;
-}
-});
-    return valid;
-}
+    console.log("return " + idDuplicated());
+    return idDuplicated();
 }
 
-    // 닉네임 유효성 검사
-    function nicknameConfirm(){
+function idDuplicated() {
+    let id = $('#memberId').val();
+    let valid = true;
+
+    $.ajax({
+        url: 'idDuplicate',
+        type: 'post',
+        data: {id: id},
+        success: function (res) {
+            if (res) {
+                $('#idmsg').css('color', 'red');
+                $('#idmsg').html("이미 사용중인 ID입니다");
+                console.log("에이젝스 flase");
+                valid = false;
+                console.log(valid);
+            } else {
+                $('#idmsg').css('color', 'blue');
+                $('#idmsg').html("사용가능한 ID입니다.");
+                console.log("에이젝스 true");
+                valid = true;
+                console.log(valid);
+            }
+        },
+        error: function () {
+            alert('IdDuplicate error');
+            console.log("에이젝스 error");
+            valid = false;
+        }
+    });
+
+    return valid;
+}
+
+// 닉네임 유효성 검사
+function nicknameConfirm(){
     //정규식 (영문/항글/숫자 2~10자, 대소문자 구분)
     nickname_regex = /^[A-Za-z가-힣0-9]{2,10}$/;
     let nickname = $('#nickname').val();
 
     if (!nickname_regex.test(nickname)) {
-    $('#nicknamemsg').css('color', 'red');
-    $('#nicknamemsg').html('닉네임은 영문,한글,숫자로 1~10글자 입력해 주세요');
-    return false;
-} else {
-    $('#nicknamemsg').css('color', 'blue');
-    $('#nicknamemsg').html('사용 가능한 닉네임입니다.');
-    return true;
-}
+        $('#nicknamemsg').css('color', 'red');
+        $('#nicknamemsg').html('닉네임은 영문,한글,숫자로 1~10글자 입력해 주세요');
+        return false;
+    } else {
+        $('#nicknamemsg').css('color', 'blue');
+        $('#nicknamemsg').html('사용 가능한 닉네임입니다.');
+        return true;
+    }
 }
 
-    // 비밀번호 유효성 검사
-    function validatePw() {
+// 비밀번호 유효성 검사
+function validatePw() {
     //비밀번호 정규표현식(영문/숫자/특수문자를 1자 이상 포함 8~15자, 대소문자 구분)
     pw_regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[~!@#$%^&*_\-+=`|\\(){}[\]:;"'<>,.?/])[a-zA-Z\d~!@#$%^&*_\-+=`|\\(){}[\]:;"'<>,.?/]{8,15}$/;
     let pw = $('#memberPw').val();
 
     if (!pw_regex.test(pw)) {
-    $('#pwmsg1').css('color', 'red');
-    $('#pwmsg1').css('font-size', '12px');
-    $('#pwmsg1').html('영문/숫자/특수문자를 1자 이상 포함한 8~15자로 입력');
-    return false;
-} else {
-    $('#pwmsg1').html("");
-    return true;
-}
+        $('#pwmsg1').css('color', 'red');
+        $('#pwmsg1').css('font-size', '12px');
+        $('#pwmsg1').html('영문/숫자/특수문자를 1자 이상 포함한 8~15자로 입력');
+        return false;
+    } else {
+        $('#pwmsg1').html("");
+        return true;
+    }
 }
 
-    // 비밀번호 일치 확인
+// 비밀번호 일치 확인
     function pwConfirm(){
     let pwConfirm = $('#pwConfirm').val();
     let pw = $('#memberPw').val();
 
     if(pwConfirm == pw){
-    $('#pwmsg').css('color', 'green');
-    $('#pwmsg').html("비밀번호 일치");
-    return true;
-} else {
-    $('#pwmsg').css('color', 'red');
-    $('#pwmsg').html("비밀번호가 일치하지 않습니다.");
-    return false;
-}
+        $('#pwmsg').css('color', 'green');
+        $('#pwmsg').html("비밀번호 일치");
+        return true;
+    } else {
+        $('#pwmsg').css('color', 'red');
+        $('#pwmsg').html("비밀번호가 일치하지 않습니다.");
+        return false;
+    }
 }
 
-    //이메일 유효성 검사 (형식+중복확인)
-    function validateEmail() {
+//이메일 유효성 검사 (형식+중복확인)
+function validateEmail() {
     //이메일 정규표현식
     email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
     let email = $('#email').val();
 
     if (!email_regex.test(email)) {
-    $('#emailmsg').css('color', 'red');
-    $('#emailmsg').html("유효하지 않은 이메일 주소입니다.");
-    $('#emCodeBt').prop('disabled', true);
-    return false;
-} else {
+        $('#emailmsg').css('color', 'red');
+        $('#emailmsg').html("유효하지 않은 이메일 주소입니다.");
+        $('#emCodeBt').prop('disabled', true);
+        return false;
+    }
     // 중복 검사
-    let valid = false;
+    console.log("return " + emailDuplicate());
+    return emailDuplicate();
+}
+
+// 이메일 중복 검사
+function emailDuplicate() {
+    let email = $('#email').val();
+    let valid = true;
+
     $.ajax({
-    url: 'emailDuplicate',
-    type: 'post',
-    data: {email: email},
-    success: function (res) {
-    if (res) {
-    $('#emailmsg').css('color', 'red');
-    $('#emailmsg').html("이미 사용중인 email입니다");
-    $('#emCodeBt').prop('disabled', true);
-    valid = false;
-} else {
-    $('#emailmsg').css('color', 'black');
-    $('#emailmsg').html("사용 가능");
-    $('#emCodeBt').prop('disabled', false);
-    valid = true;
-}
-},
-    error: function () {
-    alert('EmailDuplicate error');
-    valid = false;
-}
-});
+        url: 'emailDuplicate',
+        type: 'post',
+        data: {email: email},
+        success: function (res) {
+            if (res) {
+                $('#emailmsg').css('color', 'red');
+                $('#emailmsg').html("이미 사용중인 email입니다");
+                $('#emCodeBt').prop('disabled', true);
+                valid = false;
+            } else {
+                $('#emailmsg').css('color', 'black');
+                $('#emailmsg').html("사용 가능");
+                $('#emCodeBt').prop('disabled', false);
+                valid = true;
+            }
+        },
+        error: function () {
+            alert('EmailDuplicate error');
+            valid = false;
+        }
+    });
+
     return valid;
 }
 
-}
-
-    // 이메일 인증 번호 전송
-    function emConfirm(){
+// 이메일 인증 번호 전송
+function emConfirm(){
     let email = $('#email').val();
 
     $.ajax({
-    url: 'mailConfirm',
-    type: 'post',
-    data: {email: email},
-    success: function (code) {
-    if (code) {
-    alert("이메일이 전송되었습니다." +
-    "인증번호 확인 후 입력해주세요.")
-    chkEmailConfirm(code);
+        url: 'mailConfirm',
+        type: 'post',
+        data: {email: email},
+        success: function (code) {
+            if (code) {
+                alert("이메일이 전송되었습니다." +
+                "인증번호 확인 후 입력해주세요.");
+                chkEmailConfirm(code);
+            }
+        },
+        error: function () {
+            alert("이메일 전송에 실패하였습니다.");
+        }
+    });
 }
-},
-    error: function () {
-    alert("이메일 전송에 실패하였습니다.")
-}
-});
-}
-    // 이메일 인증 번호 확인
-    function chkEmailConfirm(code){
+// 이메일 인증 번호 확인
+function chkEmailConfirm(code){
     $('#emailconfirm').on("keyup", function(){
         if (code != $('#emailconfirm').val()) {
             emconfirmchk = false;
@@ -209,23 +230,25 @@ $(document).ready(function() {
                 "color" : "#0D6EFD",
                 "font-weight" : "bold",
                 "font-size" : "10px"
-            })
+            });
             return emconfirmchk;
         }
-    })
+    });
 }
 
-    //회원가입 폼 제출 내용 확인
+//회원가입 폼 제출 내용 확인
 function formConfirm(){
-        alert("실행은 일단 됨");
+    alert("실행은 일단 됨");
     // 아이디 유효성 검사
     if(!idConfirm()) {
         alert("유효하지 않은 아이디입니다.");
         console.log(idConfirm());
         return false;
     }
+
     if(!validatePw()) {
-        alert("유효하지 않은 비밍번호입니다.");
+        alert("유효하지 않은 비밀번호입니다.");
+        console.log(validatePw());
         return false;
     }
     if (!pwConfirm()) {
@@ -240,6 +263,7 @@ function formConfirm(){
         alert("유효하지 않은 이메일입니다.");
         return false;
     }
+
     if(!chkEmailConfirm()) {
         alert("인증번호가 일치하지 않습니다.")
     }

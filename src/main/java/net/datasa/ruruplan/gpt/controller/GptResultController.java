10961f9bbd03c7bb4683b905f74085a85f324bfb
Igodit <cijoyoung@naver.com>
2024-09-10 20:59:38 +0900

    [최종영] question.html 테스트를 위해 home.html에 빈 메뉴 링크추가, 관련 GptResultController 추가

--- src/main/java/net/datasa/ruruplan/gpt/controller/GptResultController.java
+++ src/main/java/net/datasa/ruruplan/gpt/controller/GptResultController.java
@@ -2,19 +2,53 @@ package net.datasa.ruruplan.gpt.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.datasa.ruruplan.gpt.service.GptResultService;
+import net.datasa.ruruplan.gpt.domain.dto.GptCmdDTO;
+//import net.datasa.ruruplan.gpt.service.GptResultService;
+import net.datasa.ruruplan.plan.domain.dto.PlanDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class GptResultController {
 
-    private final GptResultService gptResultService;
+//    private final GptResultService gptResultService;
 
+    /**
+     * ajax로 질문내용을 받아와서 gpt에게 질문 후 답변 내용 리턴(아직 plan테이블에 저장 x)
+     * @param gptCmdDTO 사용자의 질문 내용
+     * @return planDTO  gpt의 답변내용
+     */
 //    @ResponseBody
-//    @GetMapping("getGptPlan")
-//    public  getGptPlan(@RequestParam("cmdNum") Integer cmdNum) {
+//    @PostMapping("getGptPlan")
+//    public PlanDTO getGptPlan(@RequestBody GptCmdDTO gptCmdDTO) {
+//        PlanDTO planDTO = gptResultService.gptApi(gptCmdDTO);
+//        return planDTO;
+//    }
+
+    @GetMapping("gptView/question")
+    public String gptViewQuestion() {
+        return "gptView/question";
+    }
+
+    /**
+     * gpt답변 커스터마이징하기 버튼 누르면 plan저장 후 customPlan 페이지 로드
+     * @param planDTO
+     * @param model
+     * @return
+     */
+//    @PostMapping("customGptPlan")
+//    public String customGptPlan(@ModelAttribute PlanDTO planDTO, Model model) {
+//        gptResultService.saveGptPlan(planDTO);
+//        Integer planNum = gptResultService.getPlanNum(planDTO.getPlanNum());
+//        model.addAttribute("planNum", planNum);
+//        return "customView/customPlan";
+//    }
+
+//    @GetMapping("reGptPlan")
+//    public String reGptPlan(Model model) {
 //
 //    }
 }

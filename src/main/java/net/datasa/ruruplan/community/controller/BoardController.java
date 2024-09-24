1cc1a58888692a123366a2fa4cdb4b0dc01de911
DESKTOP-2AC9454\user <saemigwon25@gmail.com>
2024-09-24 17:56:42 +0900

    [권새미] 플랜 공유 보드 관련 틀 페이지 생성 http://localhost:8888/board 링크로 페이지 확인 가능

--- src/main/java/net/datasa/ruruplan/community/controller/BoardController.java
+++ src/main/java/net/datasa/ruruplan/community/controller/BoardController.java
@@ -1,4 +1,15 @@
 package net.datasa.ruruplan.community.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
 public class BoardController {
+
+    @GetMapping("board")
+    public String board() {
+        return "communityView/board";
+    }
 }

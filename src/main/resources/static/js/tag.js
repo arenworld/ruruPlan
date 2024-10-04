00681fa6337734d806d7ab999b358f7d22ad24b0
DESKTOP-2AC9454\user <saemigwon25@gmail.com>
2024-10-04 17:11:52 +0900

    [권새미] myplanlist.html 태그 부분,  planBoard css부분 약간 수정

---
+++ src/main/resources/static/js/tag.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.button.-regular').click(function(){
+        const btn = $(this);
+
+        const bgColor = btn.css('background-color');
+        if(bgColor === 'rgb(225, 226, 226)'){
+            cancel(btn);
+        } else {
+            search(btn);        }
+    });
+});
+
+function search(btn){
+    const text = btn.text();
+    btn.css('background-color', '#e1e2e2');
+}
+
+function cancel(btn){
+    const text = btn.text();
+    alert(text);
+}

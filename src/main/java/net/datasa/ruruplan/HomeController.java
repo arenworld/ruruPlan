016c9410873bccb8fe2e4019bc56d938e31d20bb
maya <happymomo53@gmail.com>
2024-10-07 09:00:09 +0900

    [김유나] 환율 출력 이상하게 되는거 수정

--- src/main/java/net/datasa/ruruplan/HomeController.java
+++ src/main/java/net/datasa/ruruplan/HomeController.java
@@ -14,13 +14,14 @@ import java.io.IOException;
 
             // 환율 값 불러오는 java 메소드 호출
             String value = exchangeTest.exchangeValue();
-            double exchange = Double.parseDouble(value);
-            double exchangeValue = Math.round(exchange*100) / 100.0 * 100;
+            double exchange = Double.parseDouble(value)*100;
+            double exchangeValue = Math.round(exchange*100) / 100.0;
 
             String exchangeValue2 = Double.toString(exchangeValue);
-            String text = exchangeValue2 + "₩/100￥";
+            String text = exchangeValue + "₩/100￥";
 
-            model.addAttribute("exchange", text);
+            model.addAttribute("exchangeInfo", text);
+            model.addAttribute("exchange", exchangeValue);
 
             return "home";
         }
